12af9df2149fc780a2d02988f4bcb4abf076480c
5GopenData <arlouanne@azatys.fr>
2021-12-17 17:02:12 +0100

    fichier temps produits.js

---
+++ front_end/front_end_tp1/src/models/produits.js
@@ -0,0 +1,190 @@
+export default [{
+
+    id: 'u1',
+    
+    nom: 'Bledine',
+    
+    nomMarque: 'Nutritive',
+    
+    nutrigrade:'A',
+    
+    imageUri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim.jpg',
+    
+    categorie: "Enfant",
+    
+    dateRecord:'10-20-2020',
+    
+    note:"20"
+    
+    },
+    
+    
+    
+    {
+    
+    id: 'u2',
+    
+    nom: 'Bledine',
+    
+    nomMarque: 'Nutritive',
+    
+    nutrigrade:'A',
+    
+    imageUri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim.jpg',
+    
+    categorie: "Enfant",
+    
+    dateRecord:'10-20-2020',
+    
+    note:"20"
+    
+    },{
+    
+    id: 'u3',
+    
+    nom: 'Bledine',
+    
+    nomMarque: 'Nutritive',
+    
+    nutrigrade:'A',
+    
+    imageUri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim.jpg',
+    
+    categorie: "Enfant",
+    
+    dateRecord:'10-20-2020',
+    
+    note:"20"
+    
+    },{
+    
+    id: 'u4',
+    
+    nom: 'Bledine',
+    
+    nomMarque: 'Nutritive',
+    
+    nutrigrade:'A',
+    
+    imageUri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim.jpg',
+    
+    categorie: "Enfant",
+    
+    dateRecord:'10-20-2020',
+    
+    note:"20"
+    
+    },{
+    
+    id: 'u5',
+    
+    nom: 'Bledine',
+    
+    nomMarque: 'Nutritive',
+    
+    nutrigrade:'A',
+    
+    imageUri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim.jpg',
+    
+    categorie: "Enfant",
+    
+    dateRecord:'10-20-2020',
+    
+    note:"20"
+    
+    },{
+    
+    id: 'u6',
+    
+    nom: 'Bledine',
+    
+    nomMarque: 'Nutritive',
+    
+    nutrigrade:'A',
+    
+    imageUri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim.jpg',
+    
+    categorie: "Enfant",
+    
+    dateRecord:'10-20-2020',
+    
+    note:"20"
+    
+    },{
+    
+    id: 'u7',
+    
+    nom: 'Bledine',
+    
+    nomMarque: 'Nutritive',
+    
+    nutrigrade:'A',
+    
+    imageUri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim.jpg',
+    
+    categorie: "Enfant",
+    
+    dateRecord:'10-20-2020',
+    
+    note:"20"
+    
+    },{
+    
+    id: 'u8',
+    
+    nom: 'Bledine',
+    
+    nomMarque: 'Nutritive',
+    
+    nutrigrade:'A',
+    
+    imageUri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim.jpg',
+    
+    categorie: "Enfant",
+    
+    dateRecord:'10-20-2020',
+    
+    note:"20"
+    
+    },{
+    
+    id: 'u9',
+    
+    nom: 'Bledine',
+    
+    nomMarque: 'Nutritive',
+    
+    nutrigrade:'A',
+    
+    imageUri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim.jpg',
+    
+    categorie: "Enfant",
+    
+    dateRecord:'10-20-2020',
+    
+    note:"20"
+    
+    },{
+    
+    id: 'u10',
+    
+    nom: 'Bledine',
+    
+    nomMarque: 'Nutritive',
+    
+    nutrigrade:'A',
+    
+    imageUri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim.jpg',
+    
+    categorie: "Enfant",
+    
+    dateRecord:'10-20-2020',
+    
+    note:"20"
+    
+    }
+
+    
+    ]
+    
+    
